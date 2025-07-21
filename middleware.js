f3e80427985677decb7e2c66fb3f37fddcfe752c
redspacecat <162653322+redspacecat@users.noreply.github.com>
2025-07-20 18:51:47 -0700

    fix middleware redirects

--- middleware.js
+++ middleware.js
@@ -5,14 +5,23 @@ export default function middleware(request) {
     const hostname = request.headers.get("host");
     console.log(url, hostname);
 
-    if (hostname != "w8.quuq.dev" && !hostname.endsWith(".vercel.app")) {
+    if (hostname != "w8.quuq.dev" && !hostname.endsWith(".vercel.app") && !hostname.endsWith("localhost:3000")) {
         const newHost = hostname.replace(".w8.quuq.dev", "");
-        const finalURL = `/s/${newHost}${url.pathname}`;
+        const finalURL = `/s/${newHost}${url.pathname}?rewrote=true`;
         console.log(finalURL);
         return rewrite("https://w8.quuq.dev" + finalURL);
     } else {
-        if (url.pathname.startsWith("/s/") && url.pathname.slice(3).length > 0) {
-            return Response.redirect(`https://${url.pathname.slice(3)}.w8.quuq.dev`)
+        console.log("host", hostname);
+        if (url.searchParams.has("rewrote")) {
+            console.log("request has already been rewritten");
+        } else {
+            if (url.pathname.startsWith("/s/") && url.pathname.slice(3).length > 0) {
+                console.log("request has not been rewritten already");
+                console.log("redirecting to 404");
+                url.pathname = "/404"
+                return Response.redirect(url);
+            }
         }
+        console.log("doing nothing");
     }
 }
